267fd950abeb7e75026080306a140a3a05154506
manoflogan <manoflogan@gmail.com>
2021-01-31 21:19:43 -0800

    Recycler view set up.

--- app/src/main/java/com/krishnanand/androidstarterproject/myproject/main/MainActivity.kt
+++ app/src/main/java/com/krishnanand/androidstarterproject/myproject/main/MainActivity.kt
@@ -2,9 +2,19 @@ package com.krishnanand.androidstarterproject.myproject.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.krishnanand.androidstarterproject.R
+import com.krishnanand.androidstarterproject.databinding.MainItemBinding
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -23,12 +33,83 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
         viewModelFactory
     }
 
+    private val recyclerView: RecyclerView by lazy {
+        findViewById(R.id.recycler_view)
+    }
+
+    private val emptyViewContainer: View by lazy {
+        findViewById(R.id.empty_view_container)
+    }
+
+    private val emptyRetryButton: Button by lazy {
+        emptyViewContainer.findViewById(R.id.empty_retry)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainActivityAdapter = MainActivityAdapter(viewModel)
+        viewModel.stringLiveData.observe(this, {
+          if (it == null) {
+              recyclerView.visibility = View.INVISIBLE
+              emptyViewContainer.visibility = View.VISIBLE
+          } else {
+              emptyViewContainer.visibility = View.INVISIBLE
+              recyclerView.visibility = View.VISIBLE
+              mainActivityAdapter.setStringList(it)
+              mainActivityAdapter.notifyDataSetChanged()
+          }
+        })
+        with(recyclerView) {
+            val linearLayoutManager = LinearLayoutManager(this@MainActivity)
+            layoutManager = linearLayoutManager
+            adapter = mainActivityAdapter
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(this@MainActivity, linearLayoutManager.orientation))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
         viewModel.makeAsyncRequest()
+        emptyRetryButton.setOnClickListener {
+            viewModel.makeAsyncRequest()
+        }
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+}
+
+class MainActivityViewHolder(private val mainItemBinding: MainItemBinding, private val viewModel: MainViewModel): RecyclerView.ViewHolder(mainItemBinding.root) {
+
+    fun bindViewHolder(str: String) {
+        mainItemBinding.boundString = str
+        mainItemBinding.viewModel = viewModel
+        mainItemBinding.executePendingBindings()
+    }
+}
+
+class MainActivityAdapter(private val viewModel: MainViewModel): RecyclerView.Adapter<MainActivityViewHolder>() {
+
+    private val mutableStringList = mutableListOf<String>()
+
+    public fun setStringList(stringList: List<String>) {
+       with(mutableStringList) {
+           clear()
+           addAll(stringList)
+       }
+    }
+
+    override fun getItemCount(): Int = mutableStringList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivityViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val mainItemBinding: MainItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.main_item, parent, false)
+        return MainActivityViewHolder(mainItemBinding, viewModel)
+    }
+
+    override fun onBindViewHolder(holder: MainActivityViewHolder, position: Int) {
+       holder.bindViewHolder(mutableStringList[position])
+    }
 }
--- app/src/main/java/com/krishnanand/androidstarterproject/myproject/main/MainViewModel.kt
+++ app/src/main/java/com/krishnanand/androidstarterproject/myproject/main/MainViewModel.kt
@@ -1,16 +1,33 @@
 package com.krishnanand.androidstarterproject.myproject.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.random.Random
 
 class MainViewModel @Inject constructor(
     private val mainApi: MainApi
 ): ViewModel() {
 
+    private val _stringLiveData: MutableLiveData<List<String>> = MutableLiveData()
+
+    private val randomStringData: List<String> = listOf("My", "name", "is", "Kartik", "Krishnanand")
+
+    val stringLiveData: LiveData<List<String>>
+        get() = _stringLiveData
+
     fun makeAsyncRequest() {
         viewModelScope.launch {
+            Random.nextInt(0, 2).apply {
+                if (this == 0) {
+                    _stringLiveData.postValue(null)
+                } else {
+                    _stringLiveData.postValue(randomStringData)
+                }
+            }
         }
     }
 }
